cdc24707b744cc6f593f588dda42e368b7ce2c46
Gary Magill <Garymagill22@gmail.com>
2023-04-26 11:15:38 -0600

    Initial Profile Page commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function setNewImg() {
+    document.getElementById("garyImg").src = "./Gary_Frankie2.png"
+    fade
+}
+
+function setOldImg() {
+    document.getElementById("garyImg").src = "./Gary Profile Page Pic.png"
+}
